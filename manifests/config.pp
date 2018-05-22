06d3a63f0ea0cecceb66a930f060413b78799f0b
Luis Silva <luis_silva@harvard.edu>
2018-05-22 10:22:12 -0400

    adding config file definitions

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,5 +8,18 @@ class ood::config inherits ood::params {
     mode    => 0644,
     content => template($module_name/slurm.yml.erb),
   }
-
+  file { '/etc/ood/config/ood_portal.yml':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template($module_name/ood_portal.yml.erb),
+  }
+  file { '/etc/ood/config/nginx_stage.yml':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    content => template($module_name/nginx_stage.yml.erb),
+  }
 }
--- manifests/service.pp
+++ manifests/service.pp
@@ -1,9 +1,9 @@
 # == Class: ood::service
 class ood::service inherits ood::params {
 
-  service { $ood_service_name:
-    ensure     => $ood_service_ensure,
-    enable     => $ood_service_enable,
+  service { $ood::params::ood_service_name:
+    ensure     => $ood::params::ood_service_ensure,
+    enable     => $ood::params::ood_service_enable,
     hasstatus  => true,
     hasrestart => true,
   }
